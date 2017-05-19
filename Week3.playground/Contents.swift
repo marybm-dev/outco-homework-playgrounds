439ac7c6f55ddfb7562ffb669a23e43da0c6dbee
Mary <mlmartinez85@gmail.com>
2017-05-19 10:10:56 -0700

    Complete week3day3 hw

--- Week3.playground/Contents.swift
+++ Week3.playground/Contents.swift
@@ -340,11 +340,67 @@ class HashTable {
 
 
 
+/*
+ *  Homework IX
+ *
+ *  Problem: Lattice Paths
+ *
+ *  Prompt:  Count the number of unique paths to travel from the top left
+ *           corder to the bottom right corner of a lattice of N x N squares.
+ *
+ *           When moving through the lattice, one can only travel to the adjacent
+ *           corner on the right or down.
+ *
+ *  Input:   An integer N representing the size of the lattice
+ *  Output:  An integer representing the number of unique number of paths
+ *
+ *  Example: input: 2
+ *
+ *           (2 x 2 lattice of squares)
+ *            __ __
+ *           |__|__|
+ *           |__|__|
+ *
+ *           output: 6 (number of unique paths from top left corner to bottom right)
+ *
+ *  Notes:   What is the time and auxiliary space complexity of your solution?
+ *
+ *           Try implementing your solution using recursion with side effects.
+ *
+ *  Resource:
+ *             1) https://projecteuler.net/problem=15
+ */
 
 
+/*
+ 
+ can only travel right or down
+ 
+ right down
+ down right
+ 
+ 
+ 
+ */
 
+func latticePaths(size: Int) -> Int {
 
+    func walkPath(_ x: Int, _ y: Int) -> Int {
+        var routes = 0
+        
+        if x == 0 || y == 0 {
+            routes = 1
+        } else {
+            routes = walkPath(x-1, y) + walkPath(x, y-1)
+        }
+        
+        return routes
+    }
+    
+    return walkPath(size, size)
+}
 
+latticePaths(size: 2)
 
 
 
